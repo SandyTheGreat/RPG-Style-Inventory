73b02e7e4617ce8af6078d7513579df854a5c455
Santhosh <44892614+SandyTheGreat@users.noreply.github.com>
2021-12-19 19:43:01 +0530

    Add files via upload

---
+++ Source_1.0/Sandy_Detailed_RPG_Inventory/RPG_Settings.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+using Verse;
+using RimWorld;
+
+namespace Sandy_Detailed_RPG_Inventory
+{
+    class Sandy_RPG_Settings : ModSettings
+    {
+        public static float rpgTabHeight = 500f;
+
+        public static float rpgTabWidth = 550f;
+
+        public override void ExposeData()
+        {
+            Scribe_Values.Look(ref rpgTabHeight, "rpgTabHeight", 500f);
+            Scribe_Values.Look(ref rpgTabWidth, "rpgTabWidth", 550f);
+            base.ExposeData();
+        }
+    }
+
+    class Sandy_Detailed_RPG_Inventory : Mod
+    {
+        Sandy_RPG_Settings settings;
+
+        string tabHeight;
+
+        string tabWidth;
+
+        public Sandy_Detailed_RPG_Inventory(ModContentPack content) : base(content)
+        {
+            this.settings = GetSettings<Sandy_RPG_Settings>();
+        }
+
+        public override void DoSettingsWindowContents(Rect inRect)
+        {
+            Listing_Standard listingStandard = new Listing_Standard();
+            listingStandard.Begin(inRect);
+            listingStandard.Label("RPG_Inventory_Width".Translate());
+            listingStandard.TextFieldNumeric(ref Sandy_RPG_Settings.rpgTabWidth, ref tabWidth);
+            listingStandard.Gap();
+            listingStandard.Label("RPG_Inventory_Height".Translate());
+            listingStandard.TextFieldNumeric(ref Sandy_RPG_Settings.rpgTabHeight, ref tabHeight);
+            listingStandard.End();
+            base.DoSettingsWindowContents(inRect);
+        }
+
+        public override string SettingsCategory()
+        {
+            return "RPG_Style_Inventory_Title".Translate();
+        }
+    }
+}
---
+++ Source_1.3/Sandy_Detailed_RPG_Inventory/RPG_DefOf.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Verse;
+using RimWorld;
+
+namespace Sandy_Detailed_RPG_Inventory
+{
+    [DefOf]
+    public static class Sandy_Gear_DefOf
+    {
+        public static BodyPartGroupDef Teeth;
+        public static BodyPartGroupDef Mouth;
+        public static BodyPartGroupDef Neck;
+        public static BodyPartGroupDef Shoulders;
+        public static BodyPartGroupDef Arms;
+        public static BodyPartGroupDef Hands;
+        public static BodyPartGroupDef Waist;
+        public static BodyPartGroupDef Feet;
+
+        //Jewelry
+        //Two def files were added, they are in Defs\Jewelry_compat
+        public static BodyPartGroupDef Ears;
+        public static ApparelLayerDef Accessories;
+
+        //Combat Shields
+        public static ApparelLayerDef Shield;
+
+        //UnderWhere
+        public static ApparelLayerDef UnderwearTop;
+        public static ApparelLayerDef Underwear;
+
+        //VFE
+        public static ApparelLayerDef VFEC_OuterShell;
+    }
+}
